82a4990e51509adffd908d64ea9cfb7bb73df9ec
Pachuca <luis.pachuca@uabc.edu.mx>
2023-05-02 19:51:16 -0700

    Aplicamos el formato pep8 al codigo

--- pep8.py
+++ pep8.py
@@ -1,10 +1,18 @@
-class user():
-    def __init__(self,username, password=''):
+# CamelCase
+class UserAdmin():
 
+    # Identacion: 4 espacios (No tabs)
+    # Parametros separados por la "," deben de llevar un espacio
+
+
+    def __init__(self, username, password = ''):
         self.username = username
         self.password = password
 
-        def set_password(self):
-            pass
 
-    codyuser = user('Cody')
+    def set_password(self):
+        pass
+
+# snake_case
+# espacios entre operadores
+cody_user = UserAdmin('Cody')
